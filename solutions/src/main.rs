973a7d43f99f6a4ee5e7dbdbdf8b05ab7e74041e
Marcus Näslund <marcus.naslund@kognity.com>
2025-01-10 15:09:42 +0100

    simplify runner code with macro

--- solutions/src/main.rs
+++ solutions/src/main.rs
@@ -1,68 +1,36 @@
+#![allow(dead_code)]
+
 use std::env;
 
 mod problems;
 use problems::*;
 
+macro_rules! problem_calls {
+    ($problem_id:expr, $($i:literal),*) => {
+        match $problem_id {
+            $(
+                $i => paste::paste! { [<problem $i>]::[<problem $i>]() },
+            )*
+            _ => panic!("Invalid id"),
+        }
+    };
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let arg = args.get(1).map(String::as_str).unwrap_or("-1");
+    let problem_id = args
+        .get(1)
+        .map(String::as_str)
+        .unwrap_or("-1")
+        .parse::<i32>()
+        .expect("Valid problem ID");
 
-    let problem_id = arg.parse::<i32>();
+    let result = problem_calls!(
+        problem_id, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+        23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+        46, 48, 49, 50
+    );
 
-    let result = match problem_id {
-        Ok(1) => problem1::problem1(),
-        Ok(2) => problem2::problem2(),
-        Ok(3) => problem3::problem3(),
-        Ok(4) => problem4::problem4(),
-        Ok(5) => problem5::problem5(),
-        Ok(6) => problem6::problem6(),
-        Ok(7) => problem7::problem7(),
-        Ok(8) => problem8::problem8(),
-        Ok(9) => problem9::problem9(),
-        Ok(10) => problem10::problem10(),
-        Ok(11) => problem11::problem11(),
-        Ok(12) => problem12::problem12(),
-        Ok(13) => problem13::problem13(),
-        Ok(14) => problem14::problem14(),
-        Ok(15) => problem15::problem15(),
-        Ok(16) => problem16::problem16(),
-        Ok(17) => problem17::problem17(),
-        Ok(18) => problem18::problem18(),
-        Ok(19) => problem19::problem19(),
-        Ok(20) => problem20::problem20(),
-        Ok(21) => problem21::problem21(),
-        Ok(22) => problem22::problem22(),
-        Ok(23) => problem23::problem23(),
-        Ok(24) => problem24::problem24(),
-        Ok(25) => problem25::problem25(),
-        Ok(26) => problem26::problem26(),
-        Ok(27) => problem27::problem27(),
-        Ok(28) => problem28::problem28(),
-        Ok(29) => problem29::problem29(),
-        Ok(30) => problem30::problem30(),
-        Ok(31) => problem31::problem31(),
-        Ok(32) => problem32::problem32(),
-        Ok(33) => problem33::problem33(),
-        Ok(34) => problem34::problem34(),
-        Ok(35) => problem35::problem35(),
-        Ok(36) => problem36::problem36(),
-        Ok(37) => problem37::problem37(),
-        Ok(38) => problem38::problem38(),
-        Ok(39) => problem39::problem39(),
-        Ok(40) => problem40::problem40(),
-        Ok(41) => problem41::problem41(),
-        Ok(42) => problem42::problem42(),
-        Ok(43) => problem43::problem43(),
-        Ok(44) => problem44::problem44(),
-        Ok(45) => problem45::problem45(),
-        Ok(46) => problem46::problem46(),
-        Ok(48) => problem48::problem48(),
-        Ok(49) => problem49::problem49(),
-        Ok(50) => problem50::problem50(),
-
-        _ => panic!("Invalid problem ID"),
-    };
-
-    println!("Problem {}: {}", problem_id.unwrap(), result);
+    println!("Problem {}: {}", problem_id, result);
 }
